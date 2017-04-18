c3ad861bc3c69e598bc02576f49ccb7a88e668ef
Wynand <groendrakie@gmail.com>
2017-04-18 20:58:39 +0200

    republish suite on atmosphere

--- package.js
+++ package.js
@@ -1,19 +1,21 @@
 Package.describe({
   name: 'mozfet:autoform-medium',
-  version: '1.0.0',
+  version: '2.0.4',
   summary: "Medium editor for AutoForm",
   description: "Medium editor for AutoForm",
   git: "http://github.com/djhi/meteor-autoform-medium.git"
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('METEOR@1.0');
+  api.versionsFrom('METEOR@1.4');
 
   api.use([
     'templating',
     'aldeed:autoform@6.0.0',
     'mediumeditor:mediumeditor@5.2.0',
   ], 'client');
+
+  api.use('ecmascript@0.7.2');
 
   // TODO: restore when medium editor fixe deactivate
   //api.use('tap:18n', {weak: true});
